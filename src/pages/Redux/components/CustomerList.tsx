74669d9a2194ef8e44f9b7f0c47b0f269e8a7fcd
dephiros <dephiros@gmail.com>
2023-04-10 06:28:07 -0700

    Make table scrollable

--- src/pages/Redux/components/CustomerList.tsx
+++ src/pages/Redux/components/CustomerList.tsx
@@ -1,9 +1,9 @@
-import { connect } from "react-redux";
-import { useEffect } from "preact/hooks";
-import { tw } from "twind";
+import { connect } from 'react-redux';
+import { useEffect } from 'preact/hooks';
+import { tw } from 'twind';
 
-import { fetchCustomersActionCreator } from "../ducks/customers";
-import { getCustomers } from "../selectors/customer";
+import { fetchCustomersActionCreator } from '../ducks/customers';
+import { getCustomers } from '../selectors/customer';
 
 function CustomerList({
   userId,
@@ -16,18 +16,41 @@ function CustomerList({
     fetchCustomers(userId);
   }, [userId]);
 
+  function getName(customer) {
+    return `${customer.name.first} ${customer.name.last}`;
+  }
+
   return (
     <div
-      class={tw`flex flex-col mt-5 mx-auto text-center bg-yellow-100 text-black`}
+      class={tw`flex flex-col items-center mt-5 mx-auto text-center bg-yellow-100 text-black`}
     >
       <h3 class={tw`text-xl my-3 font-bold`}>Customers</h3>
-      <ul class={tw`max-h-[500px] overflow-y-scroll`}>
-        {customers.map((customer) => (
-          <li key={customer.id}>
-            {customer.name.first}: {customer.location.country}
-          </li>
-        ))}
-      </ul>
+      <div class={tw`max-h-[500px] overflow-y-scroll`}>
+        <table class={tw`text-left min-w-[500px]`}>
+          <thead class={tw`border-b(solid 2 neutral-100) mb-6`}>
+            <tr>
+              <th class={tw`w-[50px]`}></th>
+              <th>Name</th>
+              <th>Country</th>
+            </tr>
+          </thead>
+          <tbody class="">
+            {customers.map((customer) => (
+              <tr key={customer.id}>
+                <td>
+                  <img
+                    class={tw`p-1`}
+                    alt={getName(customer)}
+                    src={customer.picture.thumbnail}
+                  />
+                </td>
+                <td>{getName(customer)}</td>
+                <td>{customer.location.country}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
