533ba4e16e50d3ac937ce99189e23b25e1118892
An Nguyen <dephiros@gmail.com>
2023-05-01 22:33:25 +0000

    complete test for customerAtom

--- src/pages/Jotai/atoms/customer.test.ts
+++ src/pages/Jotai/atoms/customer.test.ts
@@ -1,51 +1,107 @@
 import { atom, createStore } from "jotai";
-import { describe, test, vi, expect } from "vitest";
+import { describe, test, vi, expect, Mock, MockedFunction } from "vitest";
 import { JSONSchemaFaker } from "json-schema-faker";
 import { faker } from "@faker-js/faker";
 
+import { getCustomers } from "../../../api/customer";
 import { CustomerAPISchema } from "../../../interfaces/Customer";
 import { Customer } from "../../../models/Customer";
 import {
   customersAtom,
   selectedCountriesAtom,
   filteredCustomerByCountryAtom,
+  customerCountAtom,
+  customerCountriesAtom,
 } from "./customer";
 
 JSONSchemaFaker.extend("faker", () => faker);
 
-vi.mock("./customer.ts", async () => {
+vi.mock("../../../api/customer", async () => {
   // figure out how to mock only customersAtom and selectedCountriesAtom
   return {
-    ...mod,
-    customersAtom: atom<Customer[]>([]),
-    selectedCountriesAtom: atom(new Map<string, boolean>()),
+    __esModule: true,
+    getCustomers: vi.fn(),
   };
+});
+
+const mockGetCustomer = getCustomers as MockedFunction<
+  (id: string) => Promise<Array<any>>
+>;
+
+describe("test async atom", () => {
+  test("should be sub", () => {
+    const countAtom = atom(Promise.resolve(0));
+    const countPlusAtom = atom(async (get) => {
+      return (await get(countAtom)) + 1;
+    });
+    const sumAtom = atom(async (get) => {
+      return (await get(countAtom)) + (await get(countPlusAtom));
+    });
+    const store = createStore();
+    store.sub(sumAtom, async () => {
+      expect(await store.get(sumAtom)).toEqual(9);
+    });
+    store.set(countAtom, Promise.resolve(4));
+  });
 });
 
 describe("filteredCustomerByCountryAtom", () => {
   test("should return all customers if no filters", async () => {
-    const { customers, countries } = await getCountries();
-    const store = createStore();
-    store.sub(filteredCustomerByCountryAtom, (c) => {
-      //   console.log("sub", c);
-    });
-    store.set(customersAtom, Promise.resolve(customers));
-    store.set(selectedCountriesAtom, new Map());
+    const { store } = await setUpStore();
+
     const filteredCustomers = await store.get(filteredCustomerByCountryAtom);
     expect(filteredCustomers.length).toEqual(10);
   });
+  test("return customer from selected countries", async () => {
+    const { customers, countries, store } = await setUpStore();
+    const selectedCountries = countries.slice(0, 2);
+    selectedCountries.forEach((country) => {
+      store.set(selectedCountriesAtom, country, true);
+    });
+
+    const filteredCustomers = await store.get(filteredCustomerByCountryAtom);
+
+    expect(filteredCustomers.length).toEqual(
+      customers.filter((c) =>
+        countries.slice(0, 2).includes(c.location.country)
+      ).length
+    );
+  });
+  test("toggle selected returns all countries", async () => {
+    const { customers, countries, store } = await setUpStore();
+    const selectedCountries = countries.slice(0, 2);
+    selectedCountries.forEach((country) => {
+      store.set(selectedCountriesAtom, country); // toggle on
+      store.set(selectedCountriesAtom, country); // toggle off
+    });
+
+    const filteredCustomers = await store.get(filteredCustomerByCountryAtom);
+
+    expect(filteredCustomers.length).toEqual(customers.length);
+  });
 });
 
-async function getCustomers() {
+async function setUpStore() {
+  const { customers, countries } = await createCountries();
+  mockGetCustomer.mockReturnValue(Promise.resolve(customers));
+  const store = createStore();
+  store.sub(filteredCustomerByCountryAtom, () => {}); // triggle value to be calculated
+  return {
+    customers,
+    countries,
+    store,
+  };
+}
+
+async function createCustomers() {
   const customersJSON = await Promise.all(
     [...Array(10).keys()].map(() => JSONSchemaFaker.resolve(CustomerAPISchema))
   );
-  console.log("array", [...Array(10).keys()]);
   return customersJSON.map((c) => Customer.fromJS(c as any));
 }
 
-async function getCountries() {
-  const customers = await getCustomers();
+async function createCountries() {
+  const customers = await createCustomers();
   return {
     customers,
     countries: customers.map((c) => c.location.country),
